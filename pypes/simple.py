8ac29a4c159da5b09e07dc615e41b4c24846ad16
bverhoeve <46007524+bverhoeve@users.noreply.github.com>
2021-09-13 21:16:18 +0200

    basic filter

--- pypes/simple.py
+++ pypes/simple.py
@@ -1,12 +1,23 @@
 import copy
+
+
 def filter_simple(collection: list[int]) -> list[int]:
     # Iterate over all the elements in the collection
-    for i in copy(collection):
-        # If the element matches a certain 
-        if i > 0:
+    for i in copy.copy(collection):
+        # If the element matches a certain
+        if i < 0:
             collection.remove(i)
         elif i % 2 == 0:
             collection.remove(i)
-        elif i < 45:
-            collection.remove(i)
     return collection
+
+
+def main():
+    collection = list(range(-20, 20))
+    print(f"Original collection: {collection}")
+    filtered_collection = filter_simple(collection)
+    print(f"Filtered collection: {filtered_collection}")
+
+
+if __name__ == "__main__":
+    main()
